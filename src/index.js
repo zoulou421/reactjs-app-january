355f4d67341a8f067beea328082dae16e7eee290
Bonevy BEBY <bonevybeby@gmail.com>
2024-01-08 13:29:53 +0100

    testing components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Counter />
   </React.StrictMode>
 );
+
+const el = <Counter />;
+ReactDOM.render(
+  el,
+  document.getElementById("root")
+);
+
+class Counter extends React.Component {
+  state = {
+    counter: 0
+  }
+  increment = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+  render() {
+    return <div>
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <p>{this.state.counter}</p>
+      <button onClick={this.increment}>Increment</button>
+    </div>;
+  }
+}
+export default Counter
+
+function SayHello() {
+  const [name, setName] = useState("Beby");
+  return <h1>Hello {name}</h1>;
+}
+
+
+/*
+ReactDOM.render(
+  <h1>Hello Bonevy, React!</h1>,
+  document.getElementById('root')
+);*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
