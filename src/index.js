3d7a2772a867c1c2994e4d986a30ffd67053f786
Bonevy BEBY <bonevybeby@gmail.com>
2024-01-12 09:20:33 +0100

    props can be used to pass down not only state, but also functions, that may manipulate the state.

--- src/index.js
+++ src/index.js
@@ -181,6 +181,164 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+//Contact Manager
+//AddPersonForm uses state to manage the value of the text field
+function AddPersonForm() {
+  const [person, setPerson] = useState("");
+
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+        placeholder="Add new contact"
+        onChange={handleChange}
+        value={person} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+//PeopleList received an array representing the contacts and renders a list on the page:
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
+//Now we can render our components on the page and include some initial data
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+
+const el8 = (
+  <div>
+    <AddPersonForm />
+    <PeopleList data={contacts} />
+  </div>
+);
+
+
+//Finally
+
+function AddPersonForm(props) {
+  const [person, setPerson] = useState('');
+
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    props.handleSubmit(person);
+    setPerson('');
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+        placeholder="Add new contact"
+        onChange={handleChange}
+        value={person} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <PeopleList data={contacts} />
+    </div>
+  );
+}
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+
+ReactDOM.render(
+  <ContactManager data={contacts} />,
+  document.getElementById('root')
+);
+
+/*Improved**
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './style.css';
+
+function AddPersonForm(props) {
+  const [ person, setPerson ] = useState('');
+    
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+    
+  function handleSubmit(e) {
+    props.handleSubmit(person);
+    setPerson('');
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" 
+        placeholder="Add new contact" 
+        onChange={handleChange} 
+        value={person} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
+  const arr = props.data;
+  const listItems = arr.map((val, index) =>
+    <li key={index}>{val}</li>
+  );
+  return <ul>{listItems}</ul>;
+}
+
+function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <PeopleList data={contacts} />
+    </div>
+  );
+}
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+
+ReactDOM.render(
+  <ContactManager data={contacts} />, 
+  document.getElementById('root')
+);
+NB:
+An important takeaway from this lesson is that props can be used to pass down not only state, but also functions, that may manipulate the state.
+This way, we are able to store the application state in the parent and allow its child components to use and manipulate the state.
+
+Now, when our app is fully functional, we can add some CSS styles and a check, to prevent creation of blank contacts.
+*/
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
